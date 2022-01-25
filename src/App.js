6b86cdca9c7f297a6596e1e1bcf68b3dcd5bbb8b
Jxxunnn <86228307+Jxxunnn@users.noreply.github.com>
2022-01-25 18:54:49 +0900

    about component

--- src/App.js
+++ src/App.js
@@ -7,26 +7,37 @@ import "./App.css";
 function App() {
   let [글제목, 글제목변경] = useState([
     "남자 코트 추천",
-    "남자 니트 추천",
-    "남자 바지 추천",
+    "산본 혼밥 추천",
+    "안양 맛집 추천",
   ]); // [state 데이터. state 데이터 변경 함수]
 
   let [따봉, 따봉변경] = useState(0);
+
+  let [modal, modal변경] = useState(false);
+
+  function 제목바꾸기() {
+    const newArray = [...글제목];
+    newArray[0] = "여자 코트 추천";
+    newArray.sort();
+    글제목변경(newArray);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button
-        onClick={() => {
-          글제목변경(["여자 코트 추천", "여자 니트 추천", "여자 바지 추천"]);
-        }}
-      >
-        버튼
-      </button>
+
+      {/* {modal === true ? <Modal></Modal> : null} */}
+      <button>버튼</button>
+
+      <button onClick={제목바꾸기}>버튼</button>
       <div className="list">
-        <h3>
+        <h3
+          onClick={() => {
+            modal변경(true);
+          }}
+        >
           {글제목[0]}
           <span
             onClick={() => {
@@ -50,6 +61,16 @@ function App() {
         <p>2월 19일 발행</p>
         <hr />
       </div>
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
